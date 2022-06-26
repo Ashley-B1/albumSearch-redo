4d2081895aff787b32098c5f76fe6abf6009bf8e
Ashley (Skye) Brown <stormi.seuneu@outlook.com>
2022-06-25 22:22:52 -0500

    added styled album cards

--- src/Components/AlbumsContainer/Albums/index.js
+++ src/Components/AlbumsContainer/Albums/index.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
+
+import './Albums.css';
 
 const Albums = ({ album }) => {
   return (
     <div className='album-card'>
-      {album.collectionName}
+      <div className='pic-sect'>
+        <img className='album-pic' src={album.artworkUrl100} />
+      </div>
+      <h3 className='album-title'>
+        {album.collectionName} by {album.artistName}.
+      </h3>
     </div>
   )
 }
